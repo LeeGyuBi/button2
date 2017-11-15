8555187272fc17f9f3877667087e45300cb2e8b9
gyubi <rbql7687@naver.com>
2017-11-15 14:27:57 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,10 @@ window =Tk()
 
 b1 = Button(window, text="LeeGyuBi")
 b2 = Button(window, text="60162148")
+b3 = Button(window, text="970905")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
